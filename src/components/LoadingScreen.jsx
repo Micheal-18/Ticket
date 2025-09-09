8dea20debfd45859a9eb6e8bb7fd1d368e1aed32
Micheal-18 <michaeleleke259@gmail.com>
2025-09-09 15:38:10 +0100

    index favicon

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -28,7 +28,7 @@ const LoadingScreen = ({ onComplete }) => {
     return (
 
         <div className='fixed inset-0 z-50 bg-white text-orange-500 flex flex-col items-center justify-center'>
-            <div className='mb-4 text-4xl font-mono font-bold'>
+            <div className='mb-4 text-center text-4xl font-mono font-bold'>
                 {text} <span className='animate-blink ml-1'>|</span>
             </div>
 
