1ef1c0d90e79469bd9dab49e740bfc70f682c537
Micheal-18 <michaeleleke259@gmail.com>
2025-09-10 14:14:05 +0100

    typo correction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ import { FaCalendar, FaLocationArrow } from 'react-icons/fa6'
 
 const Home = () => {
 
-
   const categories = [
     {
       title: "business",
@@ -57,7 +56,7 @@ const Home = () => {
             <div data-aos="fade-up"  className='flex lg:flex-row flex-col  lg:justify-between items-center w-full bg-[#eeeeee] flex-1 gap-10 relative py-10 px-8   rounded-3xl'>
               <div className='space-y-4 flex flex-col justify-center items-center'>
                 <h1 className='uppercase text-center md:text-6xl text-4xl font-bold mt-4'>Register</h1>
-              <p className='text-gray-700 md:text-lg text-sm text-center max-w-2xl mt-2'>Join airtiks.event today and unlock a world of unforgettable experiences. Create your account now to discover, book, and attend the best events around you!</p>
+              <p className='text-gray-700 md:text-lg text-sm text-center max-w-2xl mt-2'>Join airticks.event today and unlock a world of unforgettable experiences. Create your account now to discover, book, and attend the best events around you!</p>
               </div>
 
               <div className='rounded-xl overflow-hidden'>
@@ -85,31 +84,11 @@ const Home = () => {
               </div>
             </div>
             
-          {/* <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
-            {data.map((Update) => (
-              <div className='flex justify-center items-center bg-[#eeeeee] flex-1 gap-10 relative py-10 px-8   rounded-3xl'>
-                <h1>Create Events</h1>
-                {/* <span className='space-y-2 flex flex-col'>
-                  <h1 className='font-bold uppercase text-2xl w-[150px] truncate lg:w-auto lg:whitespace-normal lg:overflow-visible'>{Update.title}</h1>
-                  <p className='md:text-lg text-sm font-regular text-gray-500 flex gap-2 items-center'><FaCalendar />{Update.date}</p>
-                  <p className='md:text-lg text-md font-regular text-gray-500 flex gap-2 items-center'><FaLocationArrow />{Update.location}</p>
-                  <span className='flex  justify-between items-end gap-4'>
-                    <p className='font-bold text-lg text-orange-500'>${Update.price}</p>
-                    <button className='bg-orange-500 p-2 rounded-lg hover:scale-105 '>Buy Ticket</button>
-                  </span> 
-                </span> 
-
-                <span className='overflow-hidden rounded-xl'>
-                  <img src={Update.image} className='object-cover md:h-50 h-30 w-50 md:w-50 hover:scale-105 duration-500 rounded-2xl' />
-                </span>
-              </div>
-            ))}
-          </div> */}
         </div>
       </section>
       <section className='flex flex-col justify-center items-center pt-10 w-full h-[50vh]'>
         <div data-aos="fade-out"  className='border-t-2 border-gray-400 '>
-          <h1 className='lg:text-9xl md:text-8xl mt-15 text-6xl font-bold'>airtiks<span className='text-orange-500'>.event</span></h1>
+          <h1 className='lg:text-9xl md:text-8xl mt-15 text-6xl font-bold'>airticks<span className='text-orange-500'>.event</span></h1>
         </div>
       </section>
     </>
